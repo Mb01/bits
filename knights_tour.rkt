f29b3201e45eb0aa7ff1078670840e56aa0d8ac8
Mark <mbowden01@gmail.com>
2018-05-15 20:25:47 +0900

    got it working

--- knights_tour.rkt
+++ knights_tour.rkt
@@ -1,4 +1,11 @@
+#!/usr/bin/env racket
 #lang racket/base
+
+
+(define empty-square -0)            
+(define side-length 8)
+(define initial-depth 0)
+(define count 0) ; debug counter
 
 ; knight moves look like this
 ; xxxxxxx (-1, 2) xxxxxxx ( 1, 2) xxxxxxx
@@ -30,10 +37,10 @@
                   ((= depth original-length) '(()))
                   ; an element can either be at the start of a permutation or not
                   (else (append 
-                   ; case in which at start, then we find all the permutations without that element
-                   (add-to-each (car li) (permutations (cdr li) 0 (sub1 original-length)))
-                   ; or not, we will rotate the list and set a different first element
-                   (permutations (rotate-left li) (add1 depth) original-length)))))]
+                         ; case in which at start, then we find all the permutations without that element
+                         (add-to-each (car li) (permutations (cdr li) 0 (sub1 original-length)))
+                         ; or not, we will rotate the list and set a different first element
+                         (permutations (rotate-left li) (add1 depth) original-length)))))]
              [multipermutations
               (lambda (li n depth)
                 (cond
@@ -50,39 +57,63 @@
              [combine; or "map" for idiots I guess
               (lambda (plus-minus one-two)
                 (letrec (
-                    [xxx
-                     (lambda (sign number)
-                       (display sign) (display number)
-                       (cond
-                         ((eq? sign '+) number)
-                         ((eq? sign '-) (- 0 number))
-                         (else 'ERROR)))]
-                     [inner
-                      (lambda (plus-minus one-two)
-                        (cond
-                          ((null? plus-minus) '())
-                          ; (... (+ -) (1 2) -> (1 -2)
-                          (else (cons
-                                 (list (xxx (car (car plus-minus)) (car one-two)) (xxx (car (cdr (car plus-minus))) (car (cdr plus-minus))))
-                                 (inner (cdr plus-minus) one-two)))))])
-                     (append
-                      (inner plus-minus (car one-two))
-                      (inner plus-minus (car (cdr one-two))))))]
-                          
+                         [interpret-sign
+                          (lambda (sign number)
+                            (cond
+                              ((eq? sign '+) number)
+                              ((eq? sign '-) (- 0 number))
+                              (else (error "we need '+ or '-"))))]
+                         [inner
+                          (lambda (plus-minus one-two)
+                            (cond
+                              ((null? plus-minus) '())
+                              ; (... (+ -)) (... (1 2)) -> (... (1 -2))
+                              (else (cons
+                                     (list (interpret-sign (car (car plus-minus)) (car one-two)) (interpret-sign (car (cdr (car plus-minus))) (car (cdr one-two))))
+                                     (inner (cdr plus-minus) one-two)))))])
+                  (append
+                   (inner plus-minus (car one-two))
+                   (inner plus-minus (car (cdr one-two))))))]
+             
              ); endletrec definitions
       (combine (multipermutations '(+ -) 2 0) (filter (lambda (x) (eq? (length x) 2)) (permutations '(1 2) 0 2)))
       )))
 
+(define all-moves (knight-moves initial-depth))
 
 
+;;;;; main function ;;;;;
 ; we're backtrack searching for a tree that goes say (side * side) deep so that we moved to every part of the board
-(define knight-tour
-  (lambda (board side-length current-x current-y depth)
-    (print-board board) ; debug
-    (cond
-      ((= depth (* side-length side-length)) (print-board board)); we've got a winner
-      )))
 
+(define knight-tour
+  (lambda (board side-length current-x current-y moves depth acc)
+    (print-board board); debug
+    ;(set! count (+ count 1))
+    ;(display count) (newline)
+    (let ([updated-x (if (null? moves) (+ 1 side-length) (+ current-x (car (car moves))))]; put it out of range if out of moves
+          [updated-y (if (null? moves)  (+ 1 side-length) (+ current-y (car (cdr (car moves)))))])
+      (cond
+        ((null? moves) #f); out of moves, go back
+        ((= depth (sub1 (* side-length side-length))) (print-board board) #f); we've got a winner; this will be encountered after placing the last move and recursing
+        ; check if the next move is valid
+        (else
+         (if (check-move board updated-x updated-y side-length)
+             (knight-tour (add-move board updated-x updated-y depth); if our move is valid, try it. or do nothing knight-tour should always return false
+                          side-length
+                          updated-x
+                          updated-y
+                          all-moves; our moves are all available again
+                          (add1 depth)
+                          acc)
+             (void))
+         ; try the rest of our moves
+         (knight-tour board
+                      side-length
+                      current-x
+                      current-y
+                      (cdr moves) ; we used up the last move
+                      depth  ; the depth is the same
+                      acc))))))
 
 (define print-board
   (lambda (board)
@@ -104,9 +135,10 @@
                     (else (cons (car board) (for-x (cdr board) (sub1 x))))))])
       (for-x board x))))
 
-; this is highly redundant
+; highly redundant way of checking whether a move is valid
 (define check-move
-  (lambda (board x y)
+  (lambda (board x y side-length)
+    ;(print-board board) (display x) (display ":") (display y)
     (letrec
         ([for-y (lambda (row y)
                   (cond
@@ -116,7 +148,7 @@
                   (cond
                     ((= x 0) (for-y (car board) y))
                     (else (for-x (cdr board) (sub1 x)))))])
-      (for-x board x))))
+      (and (> x -1) (> y -1) (< x side-length) (< y side-length) (for-x board x)))))
 
 (define empty-board
   (lambda (side-length)
@@ -128,7 +160,7 @@
               ((cons (empty-row side-length) (inner (sub1 depth-to-go))))))])
       (inner side-length))))
 
-(define empty-square -1)            
+
 
 (define empty-row
   (lambda (side-length)
@@ -137,5 +169,10 @@
       ((cons empty-square (empty-row (- side-length 1)))))))
 
 ;(knight-tour (empty-board 8) 8 0 0 0)
-;(check-move (add-move (empty-board 3) 2 2 55) 2 1)
-(knight-moves 0)
+;(check-move (add-move (empty-board side-length) 2 1 55) 2 2 side-length); -> #f
+;(check-move (add-move (empty-board side-length) 2 2 55) 2 2 side-length); -> #f
+;(knight-moves initial-depth)
+
+(knight-tour (empty-board side-length) side-length 0 0 (knight-moves initial-depth) (add1 initial-depth) '())
+
+

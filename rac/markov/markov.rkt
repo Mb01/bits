8d6d9760cc5c30f587f8aa69d9b0ee654918c0fa
Mark <mbowden01@gmail.com>
2018-06-22 14:51:42 +0900

    refactor

--- rac/markov/markov.rkt
+++ rac/markov/markov.rkt
@@ -7,7 +7,9 @@
 ;; markov-generate-text tends to move towards later in the text
 ;; the longer the input, the less chance of running into the end
 ;; TODO: handle reaching the end more gracefully
-(define read-n-chars 300000)
+(define read-n-chars 1000000)
+
+(define in (open-input-file input-file))
 
 ;; adds item to list for key, initializes list for key if key not in ht
 (define (update ht key item); -> alters hash table
@@ -24,8 +26,6 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; prep data 
 
-(define in (open-input-file input-file));
-
 (define (into-words s); -> li
   (define (replace-punc s); adds space before punctuation so we can split by space
     (regexp-replace* #rx"([.,:;?!])" s " \\1"))
@@ -40,45 +40,35 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; generate a markov chain
 
-(define (generate-markov-chain ht li n-key n-value); -> alters hash table "ht"
+(define (generate-markov-chain ht li key-length); -> alters hash table "ht"
   (cond
-    ((< (length li) (+ n-key n-value)) li); -> return remaining words for checking
+    ((< (length li) (add1 key-length))
+     (update ht (take li key-length) 'EOF))
     (else
-     (let ([key-word (take li n-key)]
-           [value-words (take (drop li n-key) n-value)])
-       (update ht key-word value-words)
-       (generate-markov-chain ht (cdr li) n-key n-value)))))
+     (let ([key-words (take li key-length)]
+           [value-word (car (drop li key-length))])
+       (update ht key-words value-word)
+       (generate-markov-chain ht (cdr li) key-length)))))
 
-;; TODO: try to avoid a dead end
-;; it seems that the hash table creation gets slow with increased size
-;; maybe we can make more hash tables containing more of the text
-;; then we can keep trying to revert to the first hash table
-;; then we can reduce time and avoid dead ends
-(define (markov-generate-text ht n-words n-key key); -> (s1 s2 s3...)
-  ;; n-words how many words to generate
-  ;; n-key, how long is the key
-  ;; last-n, what were last n words for the key
+(define (markov-generate-text ht n-words key); -> (word1 word2 word3...)
   (cond
     ((zero? n-words) '())
-    ;; cons the chosen word onto the list and
-    ;; recurse with updated last n
     (else
      (let* ([next (choose-random (hash-ref ht key))]
-            [new-key (append (cdr key) next)])
-       (cons
-        (car next)
-        (markov-generate-text ht (sub1 n-words) n-key new-key))))))
+            [new-key (append (cdr key) (list next))])
+       (cons next (markov-generate-text ht (sub1 n-words) new-key))))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; driver
 
 (define ht (make-hash))
-(define lead-length 3)
+(define key-length 3)
+(define n-words 10000)
 (define taste (into-words (read-string read-n-chars in)))
 
-(generate-markov-chain ht taste lead-length 1)
+(generate-markov-chain ht taste key-length)
 
 ;; choose three words in the text that are not near the end
-(display (fix-punctuation (string-join (markov-generate-text ht 10000 lead-length '("Will" "be" "a")))))
+(display (fix-punctuation (string-join (markov-generate-text ht n-words '("Will" "be" "a")))))
 
 

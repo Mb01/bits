8e68b5d569eeaf78ff530626703b8d3693aee7b0
Mark <mbowden01@gmail.com>
2018-06-25 16:27:36 +0900

    problem solved

--- rac/euler/substring_divisibility.rkt
+++ rac/euler/substring_divisibility.rkt
@@ -11,93 +11,58 @@ Let d1 be the 1st digit, d2 be the 2nd digit, and so on. In this way, we note th
     d7d8d9=728 is divisible by 13
     d8d9d10=289 is divisible by 17
 
-Find the sum of all 0 to 9 pandigital numbers with this property.
+Find the sum of all 0 to 9 pan-digital numbers with this property.
 |#
+
+;; ok, we know these are not amenable to brute force anyway,
+;; another approach is to build the numbers from the left and back track
 
 #lang racket
 
-(require "utils.rkt")
+(require "utils.rkt") ; for integer->list / list->integer
 
-; one approach is brute force all permutations
-; but its too slow
 (define start '(9 8 7 6 5 4 3 2 1 0)) ; we'll go backwards to avoid losing the zero early
 
-(define div-list '(2 3 5 7 11 13 17))
+;; added ones to beginning to check "" d1 d1d2 d1d2d3
+(define div-list '(1 1 1 2 3 5 7 11 13 17))
 
-(define (drop1 li)
-  (drop li 1))
-
-(define (take3 li)
-  (if (< (length li) 3)
-      (take3 (cons 0 li)); this makes me happy
-      (take li 3)))
-
-(define (divisible? li x)
-  (= (modulo (list->integer li) x) 0))
-
-(define (has-property? li)
-  (letrec
-      (
-       [inner
-        (lambda (li div-list)
-          (cond
-            ((null? div-list) #t)
-            ((not (divisible? (take3 li) (car div-list))) #f)
-            (else (inner (drop1 li) (cdr div-list)))))])
-    (inner (drop1 li) div-list)))
-
-
-(define (search li)
-    (let ([next (integer->list (prev-permutation (list->integer li)))])
+;; returns last elements in a list, or at least three numbers when list not long enough
+(define (last-three li)
   (cond
-   ((has-property? li))
-   ((cons (list->integer li) (search next)))
-   (else
-    (search next)))))
+    ((< (length li) 3) '(1 2 3))
+    ((= (length li) 3) li)
+    (else (last-three (cdr li)))))
 
+;; removes el from li
+(define (remove-member li el)
+  (cond
+    ((null? li) '())
+    ((= (car li) el) (remove-member (cdr li) el))
+    (else (cons (car li) (remove-member (cdr li) el)))))
 
-; ok, so what are we left with
-; another approach is to build the numbers three digits at a time from the left
+(define (search starting-numbers div-list)
+  
+  ;; numbers move from remaining to acc as used
+  ;; try is number of tries and indice across remaining
+  ;; div-list has the current number by which (last-three li) must be divisible
+  (define (helper remaining try div-list acc)
+    (cond
+      ((null? remaining) (list (list->integer acc))) ; answer found
+      ((= try (length remaining)) (list)) ; check if we exhausted tries
+      (else ;; examine accumulator after adding value, and continue trying
+       (let* (;; element to try
+              [try-el (list-ref remaining try)]
+              ;; accumulator with new value added
+              [new-acc (append acc (list try-el))])
+         (append
+          (if ; we fulfill the divisibility requirement with new-acc
+           (zero? (modulo (list->integer (last-three new-acc)) (car div-list)))
+           ;;remove tried el, reset try number, iter div-list, update acc and recurse
+           (helper (remove-member remaining try-el) 0 (cdr div-list) new-acc)
+           (list))
+          ;; keep trying with old accumulator
+          (helper remaining (add1 try) div-list acc))))))
+  (helper starting-numbers 0 div-list '()))
 
+(apply + (search start div-list))
 
-(define start2 (set '1 2 3 4 5 6 7 8 9 0))
-
-(define (n-choose-3 remaining-list)
-  (letrec
-      (
-       [inner
-        (lambda (li acc)
-          (cond
-            ((= 3 (length acc)) (list acc))
-            ((null? li) '())
-            ; choose or not choose first number
-            (else
-             (append
-              (inner (cdr li) (cons (car li) acc))
-              (inner (cdr li) acc)))))])
-    (inner (qsort remaining-list) '())))
-
-(define (all-3-permutes remaining-set)
-  (letrec
-      ([do-one
-        (lambda (perm)
-          (let ([next (prev-permutation (list->integer perm))])
-          (if next
-              (cons (integer->list next) (do-one (integer->list next)))
-              (list))))])
-    (apply append (map do-one (n-choose-3 (set->list remaining-set))))))
-
-;(define (build remaining-set div-list acc)
-
-; well we could go down that route but I have a better idea
-; let's descend into the tree with pruning
-
-(define (slice li from to)
-  (letrec ([inner
-            (lambda (count acc)
-              if (= count to)
-
-(define (build remaining acc) ; the ugly way
-  (cond 
-    (or
-     (and (= length acc 4) (not  

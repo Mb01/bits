ec9ceb8b7df2fdcfbecc8acbcd3b60dd4936649b
Mark <mbowden01@gmail.com>
2018-07-18 11:56:48 +0900

    cleaned up

--- rac/other/practice.rkt
+++ rac/other/practice.rkt
@@ -4,20 +4,21 @@
   (map (curry cons x) li))
 
 (define (combinations li)
-  (cond
-    [(null? li) '(())]
-    [else
-     (append
-      (cons-each (car li) (combinations (cdr li)))
-      (combinations (cdr li)))]))
+  (if (null? li) '(())
+      (append
+       (cons-each (car li) (combinations (cdr li)))
+       (combinations (cdr li)))))
 
 (define (one-others li)
   ;; the idea for permutations
   (map (λ (x) (list x (remove x li))) li))
 
-(define (permutations li)
-  (cond
-    [(null? li) '(())]
-    [else
-     (append-map
-      (λ (x) (cons-each x (permutations (remove x li)))) li)]))
+
+;; for each el, cons it to permutations achieved by
+;; calling permutations without it
+;; append these lists together
+(define (permutations li) ; list-> list of list
+  (if (null? li) '(())
+      (append-map
+       (λ (el)
+         (cons-each el (permutations (remove el li)))) li)))

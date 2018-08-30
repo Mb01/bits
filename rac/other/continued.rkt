3901351c7bc96dea3fb0fe496200775e5917e24d
mb01 <mbowden01@gmail.com>
2018-08-30 01:53:00 -0500

    exploring continued fractions

---
+++ rac/other/continued.rkt
@@ -0,0 +1,28 @@
+#lang racket
+
+(define n (* 2 3 5 7)); 210
+(define d (* 2 11 13)) ; 286
+(/ n d) ; 105 / 143
+
+;; euclids algorithm works like this
+(define (gcd n d)
+  (displayln `(,n ,d))
+  (let ([remainder (remainder n d)])
+  (cond
+    [(zero? remainder) d]
+    [else (gcd d remainder)])))
+
+(gcd n d)
+(newline)
+
+;; a continued fraction works like this
+
+(define (cont n d)
+  (displayln `(,n ,d))
+  (let ([quotient (quotient n d)]
+        [remainder (remainder n d)])
+    (cond
+      [(zero? remainder) d]
+      [else (cont remainder (- d remainder))])))
+    
+(cont n d)
